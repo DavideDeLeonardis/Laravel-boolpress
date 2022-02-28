aa2f64f65da62ca7e1e8136960d43fc627ab8543
Davide De Leonardis <davidedeleonardis1711@gmail.com>
2022-02-28 22:20:22 +0100

    first commit

---
+++ database/seeds/PostSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Model\Post;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 100; $i++) {
+            $newPost = new Post();
+            $newPost->title = $faker->sentence(3, true);
+            $newPost->content = $faker->paragraphs(5, true);
+            $newPost->slug = Str::slug($newPost->title . '-' . $i, '-');
+            $newPost->save();
+        }
+    }
+}
