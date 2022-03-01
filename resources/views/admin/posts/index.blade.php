5a3828be8f8d87c9600c261bf6e86a83552ca73d
Davide De Leonardis <davidedeleonardis1711@gmail.com>
2022-03-01 17:54:46 +0100

    add explode view index, show

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,1 +1,32 @@
-@dd($posts)
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            @if (session('status'))
+                <div class="alert alert-danger">
+                    {{ session('status') }}
+                </div>
+            @endif
+        </div>
+        <div class="row">
+            <div class="col">
+                {{ $posts->links() }}
+            </div>
+        </div>
+        @foreach ($posts as $post)
+            <div class="row">
+                <div class="col">
+                    <h1>
+                        {{ $post->title }}
+                    </h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    {{ $post->content }}
+                </div>
+            </div>
+        @endforeach
+    </div>
+@endsection
