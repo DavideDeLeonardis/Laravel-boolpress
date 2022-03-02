f038cb27ca90e2f3fa9f299ba35fdab2c696eae1
Davide De Leonardis <davidedeleonardis1711@gmail.com>
2022-03-02 16:10:14 +0100

    model, migration, seeder UserInfo

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,14 @@ Route::middleware('auth')
             ->name('home');
         Route::resource('posts', 'PostController');
     });
+
+// Create una tabella UserInfos e aggiungete una relazione tra Posts e Users come fatto in classe
+// User one to one Userinfo
+// User one to many Post
+// Poi aggiungete i relativi seeder come fatto assieme e aggiornate la crud Post
+// Fatto questo dedicatevi alla parte frontend
+
+// aggiungete
+// model, migration, seeder di Category
+// Aggiungete relazione one to many con Post
+// Modificate create ed edit per inserire le select con le categorie
