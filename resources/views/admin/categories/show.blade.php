adc6461f16ca6940fb84bd82dcac490fa7c8539b
Davide De Leonardis <davidedeleonardis1711@gmail.com>
2022-03-07 21:01:51 +0100

    improve categories

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -20,23 +20,22 @@
             <table class="table table-striped">
                 <thead>
                     <tr>
-                        <th scope="col">Id</th>
-                        <th scope="col">Name</th>
+                        <th scope="col">Editor Name</th>
+                        <th scope="col">Post Name</th>
                         <th scope="col">Created At</th>
                         <th scope="col">Updated At</th>
-                        <th colspan="3" scope="col">Actions</th>
+                        {{-- <th colspan="3" scope="col">Actions</th> --}}
                     </tr>
                 </thead>
 
                 <tbody>
                     @foreach ($category->posts()->get() as $post)
                         <tr>
-                            <td>{{ $post->id }}</td>
+                            <td>{{ $post->user()->first()->name }}</td>
                             <td>{{ $post->title }}</td>
                             <td>{{ $post->created_at }}</td>
                             <td>{{ $post->updated_at }}</td>
-                            <td><a class="btn btn-primary" href="{{ route('admin.posts.show', $post->slug) }}">View</a>
-                            </td>
+                            {{-- <td><a class="btn btn-primary" href="{{ route('admin.posts.show', $post->slug) }}">View</a></td> --}}
                         </tr>
                     @endforeach
                 </tbody>
